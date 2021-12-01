73d357b6702a591aa4a490beec91f30fa559b449
Andreas Olf <a.olf@it-michel.de>
2021-12-01 13:31:19 +0100

    Day 1 Part 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventOfCode
 {
     internal class Program
     {
+        private static List<string> GetInput()
+        {
+            return File.ReadAllLines("input.txt").ToList();
+        }
+
         static void Main(string[] args)
         {
+            var previousNumber = int.MaxValue;
+            int counter = 0;
+            List<string> input = GetInput();
+            foreach (var item in input)
+            {
+                var currentNumber = int.Parse(item);
+                if(currentNumber > previousNumber)
+                {
+                    counter++;
+                }
+                previousNumber = currentNumber;
+            }
+            Console.WriteLine(counter.ToString());
+            Console.ReadKey();
         }
     }
 }
