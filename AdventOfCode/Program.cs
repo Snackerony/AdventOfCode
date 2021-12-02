2508d74bf6e1a6005f4fdbac16240a52ca3a0a77
Andreas Olf <a.olf@it-michel.de>
2021-12-02 16:38:55 +0100

    Day 2 Part 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -16,21 +16,29 @@ namespace AdventOfCode
 
         static void Main(string[] args)
         {
-            var previousNumber = int.MaxValue;
-            int counter = 0;
-            string[] input = GetInput().ToArray();
-            for (int i = 0; i < input.Length; i++)
+            List<string> input = GetInput();
+            int depth = 0;
+            int forward = 0;
+            foreach (var item in input)
             {
-                if (i + 2 >= input.Length) break;
-                var currentNumber = int.Parse(input[i]) + int.Parse(input[i + 1]) + int.Parse(input[i + 2]);
-                if (currentNumber > previousNumber)
-                {
-                    counter++;
+                string[] line = item.Split(' ');
+                switch (line[0]) {
+                    case "forward":
+                        forward += int.Parse(line[1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(line[1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(line[1]);
+                        break;
+                        default:
+                        break;
                 }
-                previousNumber = currentNumber;
             }
-            Console.WriteLine(counter.ToString());
-            Console.ReadKey();
+            Console.WriteLine($"{depth * forward}");
+            Console.ReadKey();  
+
         }
     }
 }
