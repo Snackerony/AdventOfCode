d4fb54a4e864330800567e9e6490505f9b38cb4e
Andreas Olf <a.olf@it-michel.de>
2021-12-19 01:20:45 +0100

    Day 7 Part 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -16,63 +16,31 @@ namespace AdventOfCode
 
         static void Main(string[] args)
         {
-            int days = 256;
             List<int> input = GetInput().First().Split(',').ToList().ConvertAll(int.Parse);
-            List<FishCountSaver> saver = new List<FishCountSaver>();
-            long fishCount = 0;
-            foreach (var item in input)
+            List<CrabCounter> counter = new List<CrabCounter>();
+            input.Sort();
+            for (int i = input.First(); i <= input.Last(); i++)
             {
-                if (saver.Find(m => m.startFishes == item && m.daysLeft == days) == null)
+                int singleCounter = 0;
+                foreach (var item in input)
                 {
-                    long prod = ProducesFish(item, days, ref saver);
-                    saver.Add(new FishCountSaver()
-                    {
-                        producedFishes = prod,
-                        startFishes = item,
-                        daysLeft = days
-                    });
+                    singleCounter += Math.Max(i, item) - Math.Min(i, item);
                 }
-                fishCount += saver.Find(m => m.startFishes == item && m.daysLeft == days).producedFishes;
+                counter.Add(new CrabCounter(singleCounter, i));
             }
-            Console.WriteLine(fishCount);
+            counter.Sort((i,j) => i.fuel.CompareTo(j.fuel));
+            Console.WriteLine($"{counter.First().position} {counter.First().fuel}");
             Console.ReadLine();
         }
-
-        static long ProducesFish(int lifeCycle, int daysLeft, ref List<FishCountSaver> saver)
-        {
-            long fishes = 1;
-            while (daysLeft > 0)
-            {
-                if (lifeCycle == 0)
-                {
-                    lifeCycle = 6;
-                    if (saver.Find(m => m.startFishes == 8 && m.daysLeft == (daysLeft - 1)) == null)
-                    {
-                        long prod = ProducesFish(8, (daysLeft - 1), ref saver);
-                        saver.Add(new FishCountSaver()
-                        {
-                            producedFishes = prod,
-                            startFishes = 8,
-                            daysLeft = (daysLeft - 1)
-                        });
-                    }
-                    fishes += saver.Find(m => m.startFishes == 8 && m.daysLeft == (daysLeft - 1)).producedFishes;
-                }
-                else
-                {
-                    lifeCycle--;
-                }
-                daysLeft--;
-            }
-            //Console.WriteLine(lifeCycle);
-            return fishes;
-        }
     }
-
-    class FishCountSaver
+    class CrabCounter
     {
-        public int startFishes;
-        public long producedFishes;
-        public int daysLeft;
+        public CrabCounter(int fuel, int position)
+        {
+            this.fuel = fuel;
+            this.position = position;
+        }
+        public int fuel;
+        public int position;
     }
 }
